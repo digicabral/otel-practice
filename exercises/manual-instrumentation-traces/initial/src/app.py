47f265c0f6794688f8eccbbefe17fb240633b6bd
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2025-05-07 12:21:56 -0300

    feat: manual instrumentation traces

--- exercises/manual-instrumentation-traces/initial/src/app.py
+++ exercises/manual-instrumentation-traces/initial/src/app.py
@@ -1,15 +1,34 @@
 # pyright: reportMissingTypeStubs=false, reportUnknownParameterType=false, reportMissingParameterType=false, reportUnknownArgumentType=false, reportUnknownMemberType=false, reportAttributeAccessIssue=false
 
 import time
-
+import json
 import requests
 from client import ChaosClient, FakerClient
-from flask import Flask, make_response
+from flask import Flask, make_response, request
+from trace_utils import create_tracer
+from opentelemetry import trace as trace_api
+from opentelemetry.semconv.trace import SpanAttributes
+from opentelemetry.propagate import inject, extract
+from opentelemetry import context
 
 # global variables
 app = Flask(__name__)
+tracer = create_tracer("app.py", "0.1")
+
+@app.teardown_request
+def teardown_request_func(err):
+    previous_ctx = request.environ.get("previous_ctx_token", None)
+    if previous_ctx:
+        context.detach(previous_ctx)
+
+@app.before_request
+def before_request_func():
+    ctx = extract(request.headers)
+    previous_ctx = context.attach(ctx)
+    request.environ["previous_ctx_token"] = previous_ctx
 
 @app.route("/users", methods=["GET"])
+@tracer.start_as_current_span("users")
 def get_user():
     user, status = db.get_user(123)
     data = {}
@@ -18,16 +37,32 @@ def get_user():
     response = make_response(data, status)
     return response
 
-
+@tracer.start_as_current_span("do_stuff")
 def do_stuff():
+    headers = {}
+    inject(headers)
     time.sleep(0.1)
     url = "http://localhost:6000/"
     response = requests.get(url)
+    # debug
+    print("Headers included in outbound request:")
+    print(json.dumps(response.json()["request"]["headers"], indent=2))
+    response = requests.get(url, headers=headers)
     return response
 
 
 @app.route("/")
+@tracer.start_as_current_span("index")
 def index():
+    span = trace_api.get_current_span()
+    span.set_attributes(
+        {
+            SpanAttributes.HTTP_REQUEST_METHOD: request.method,
+            SpanAttributes.URL_PATH: request.path,
+            SpanAttributes.HTTP_RESPONSE_STATUS_CODE: 200,
+        }
+    )
+    
     do_stuff()
     current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
     return f"Hello, World! It's currently {current_time}"
