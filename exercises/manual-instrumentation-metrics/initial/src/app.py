d62531052a07940266e56185c781516067ef790d
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2025-05-08 15:43:12 -0300

    feat: manual instrumentation metrics

--- exercises/manual-instrumentation-metrics/initial/src/app.py
+++ exercises/manual-instrumentation-metrics/initial/src/app.py
@@ -1,13 +1,38 @@
-# pyright: reportMissingTypeStubs=false, reportUnknownParameterType=false, reportMissingParameterType=false, reportUnknownArgumentType=false, reportUnknownMemberType=false, reportAttributeAccessIssue=false
-
 import time
 
 import requests
 from client import ChaosClient, FakerClient
-from flask import Flask, make_response
+from flask import Flask, Response, make_response, request
+from metric_utils import (
+    create_meter,
+    create_request_instruments,
+    create_resource_instruments,
+)
 
 # global variables
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    workload_instruments["traffic_volume"].add(
+        1, attributes={"http.route": request.path}
+    )
+    request.environ["request_start"] = time.time_ns()
+
+
+@app.after_request
+def after_request(response: Response) -> Response:
+    workload_instruments["request_latency"].record(
+        amount=(time.time_ns() - request.environ["request_start"]) / 1_000_000_000,
+        attributes={
+            "http.request.method": request.method,
+            "http.route": request.path,
+            "http.response.status_code": response.status_code,
+        },
+    )
+    return response
+
 
 @app.route("/users", methods=["GET"])
 def get_user():
@@ -26,13 +51,19 @@ def do_stuff():
     return response
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     do_stuff()
     current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
+    # workload_instruments['traffic_volume'].add(1, {"result": choice(['win', 'lose', 'draw']) })
     return f"Hello, World! It's currently {current_time}"
 
 
 if __name__ == "__main__":
+    # setup metrics
+    meter = create_meter("app.py", "0.1")
+    workload_instruments = create_request_instruments(meter)
+    rc_instruments = create_resource_instruments(meter)
+
     db = ChaosClient(client=FakerClient())
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=False)
