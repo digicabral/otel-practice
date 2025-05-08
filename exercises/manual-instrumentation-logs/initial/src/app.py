b6f5b27fe74c18a9170272c6c37c4c669e770f68
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2025-05-08 16:36:18 -0300

    feat: manual instrumentation logs

--- exercises/manual-instrumentation-logs/initial/src/app.py
+++ exercises/manual-instrumentation-logs/initial/src/app.py
@@ -1,10 +1,19 @@
 # pyright: reportMissingTypeStubs=false, reportUnknownParameterType=false, reportMissingParameterType=false, reportUnknownArgumentType=false, reportUnknownMemberType=false, reportAttributeAccessIssue=false
 
 import time
-
+import logging
+from logging_utils import handler
 import requests
 from client import ChaosClient, FakerClient
 from flask import Flask, make_response
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(process)d - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger()
+logger.addHandler(handler)
 
 # global variables
 app = Flask(__name__)
@@ -12,10 +21,15 @@ app = Flask(__name__)
 @app.route("/users", methods=["GET"])
 def get_user():
     user, status = db.get_user(123)
+    logging.info(f"Found user {user!s} with status {status}")
     data = {}
     if user is not None:
         data = {"id": user.id, "name": user.name, "address": user.address}
+    else:
+        logging.warning(f"Could not find user with id {123}")
+    logging.debug(f"Collected data is {data}")
     response = make_response(data, status)
+    logging.debug(f"Generated response {response}")
     return response
 
 
@@ -28,6 +42,7 @@ def do_stuff():
 
 @app.route("/")
 def index():
+    logging.info("Info from the index function")
     do_stuff()
     current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
     return f"Hello, World! It's currently {current_time}"
